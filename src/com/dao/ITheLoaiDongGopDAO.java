565c607606be5f4c53ccd28e6a1fffa132146d3d
Neeyud <alberteinstein825@gmail.com>
2020-12-15 23:47:01 +0700

    thêm thể loại đóng góp DAO

---
+++ src/com/dao/ITheLoaiDongGopDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao;
+
+import com.model.TheLoaiDongGopModel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+/**
+ *
+ * @author VITQUAY
+ */
+public interface ITheLoaiDongGopDAO extends IGenericDAO<TheLoaiDongGopModel> {
+
+    List<TheLoaiDongGopModel> findAll();
+
+    void insert(TheLoaiDongGopModel newTL);
+
+    void update(TheLoaiDongGopModel updateTL);
+
+    void delete(int maTL);
+    
+    TheLoaiDongGopModel findByMa(int maTL);
+    
+    Vector findTL(ArrayList<TheLoaiDongGopModel> listTL);
+}
