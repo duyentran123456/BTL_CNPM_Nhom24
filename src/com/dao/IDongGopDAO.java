7f220dd5a35940129711e426d37442c6af61f3ae
RV12309 <shinku.izumi111@gmail.com>
2020-12-15 19:58:45 +0700

    thêm các lớp DAO đóng góp

---
+++ src/com/dao/IDongGopDAO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dao;
+
+import com.model.DongGopModel;
+import java.util.List;
+
+/**
+ *
+ * @author VITQUAY
+ */
+public interface IDongGopDAO extends IGenericDAO<DongGopModel> {
+
+    List<DongGopModel> findAll();
+
+    DongGopModel findByDG(int maDG);
+    
+    List<DongGopModel> findByTheLoai(int maTL);
+    
+    List<DongGopModel> findByMaHK(int maHK);
+
+    void insert(DongGopModel newDG);
+
+    void update(DongGopModel updateDG);
+    
+    void delete(int maDG);
+}
